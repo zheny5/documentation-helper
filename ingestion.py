debbf1012232fd2690875d56a9e5f87f43468796
zheny5 <17645002906@163.com>
2025-12-04 21:46:23 +0800

    Switch to HuggingFace embeddings

--- ingestion.py
+++ ingestion.py
@@ -5,10 +5,11 @@ from typing import Any, Dict, List
 
 import certifi
 from dotenv import load_dotenv
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
 
@@ -22,15 +23,23 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
 
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-small",
-    show_progress_bar=False,
-    chunk_size=50,
-    retry_min_seconds=10,
-)
+# embeddings = OpenAIEmbeddings(
+#     model="text-embedding-3-small",
+#     show_progress_bar=False,
+#     chunk_size=50,
+#     retry_min_seconds=10,
+# )
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    encode_kwargs={
+        "batch_size": 50,                   # 对应 chunk_size
+        "normalize_embeddings": True        # 标准化向量
+    }
+) 
 chroma = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
 vectorstore = PineconeVectorStore(
-    index_name="langchain-docs-2025", embedding=embeddings
+    index_name="langchain-docs", embedding=embeddings
 )
 tavily_extract = TavilyExtract()
 tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
